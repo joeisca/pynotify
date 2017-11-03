f6980a9f123a6a8d3df33bfc5462b5df8490674b
Joe Isca <joeisca@gmail.com>
2017-11-03 07:13:07 -0500

    moved version refernence to __init__

--- setup.py
+++ setup.py
@@ -5,39 +5,15 @@
 
 
 import os
-import subprocess
-import sys
 
+from pynotifyx import __version__ as version
+from setuplib import setup
 from setuptools import Extension
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'modules'))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__)))
-
-from setuplib import setup
-
-
-def get_version(release_file):
-    try:
-        f = open(release_file, 'r')
-        try:
-            return f.read().strip()
-        finally:
-            f.close()
-    except (IOError, OSError):
-        try:
-            output = subprocess.check_output(['bzr', 'revno'])
-            return 'bzr' + output.strip()
-        except Exception:
-            pass
-    return 'unknown'
-
-
-version = get_version('release')
 
 setup(
     name='pynotifyx',
-    distinfo_module='pynotifyx.distinfo',
-    version=version,
+    version='.'.join(map(str, version)),
     description='Simple Linux inotify bindings',
     author='Joe Isca',
     author_email='joeisca@gmail.com',
@@ -45,7 +21,7 @@ setup(
     packages=['pynotifyx'],
     entry_points={
         "console_scripts": [
-            "pynotifyx=pynotifyx:main",
+            "pynotifyx=pynotifyx.__main__:main",
         ]
     },
     ext_modules=[
